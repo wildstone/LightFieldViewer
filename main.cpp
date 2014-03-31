abb1b083c915e93ce3a469d0d59e8db84e1377d7
croft123 <wmh199014@gmail.com>
2014-03-31 13:10:10 -0400

    1st commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include <iostream>
+#include "opencv2/core/core.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/calib3d/calib3d.hpp>
+#include "opencv2/nonfree/features2d.hpp"
+#include <opencv2/core/core.hpp>
+
+
+
+
+#include <stdlib.h>
+//#include <windows.h>
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <math.h>
+
+
+
+
+using namespace std;
+using namespace cv;
+
+int main( int argc, char** argv )
+{
+
+}
